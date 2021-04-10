03c818898131afbfa83967606503d70378a75655
Schneider József <jozsef.schneider1996@gmail.com>
2021-04-10 18:00:46 +0200

    'AddedXMLResources'

---
+++ app/src/main/java/com/example/lablyrics/ui/lyrics/ShowLyricsActivity.kt
@@ -0,0 +1,16 @@
+package com.example.lablyrics.ui.lyrics
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ShowLyricsActivity: AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        //textview.setMovementMethod(new ScrollingMovementMethod())
+    }
+
+
+}
