0df8fe248fedb1be77ba0ca0c48af4d4f08d0408
Schneider József <jozsef.schneider1996@gmail.com>
2021-05-11 20:32:54 +0200

    added Crashlytics

--- app/src/main/java/com/example/lablyrics/ui/about/AboutActivity.kt
+++ app/src/main/java/com/example/lablyrics/ui/about/AboutActivity.kt
@@ -2,11 +2,15 @@ package com.example.lablyrics.ui.about
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.lablyrics.R
 import com.example.lablyrics.databinding.ActivityAboutBinding
 import com.example.lablyrics.databinding.ActivitySearchBinding
 import com.example.lablyrics.ui.search.SearchActivity
+import com.google.firebase.FirebaseApp
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 
 class AboutActivity : AppCompatActivity() {
     private lateinit var binding : ActivityAboutBinding
@@ -21,5 +25,19 @@ class AboutActivity : AppCompatActivity() {
             val intent = Intent(applicationContext, SearchActivity::class.java)
                 startActivity(intent)
         }
+
+        FirebaseApp.initializeApp(applicationContext);
+        FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(true);
+
+        val crashButton = Button(this)
+        crashButton.text = "Crash!"
+        crashButton.setOnClickListener {
+            throw RuntimeException("Test Crash") // Force a crash
+        }
+
+        addContentView(crashButton, ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT))
+
     }
 }
